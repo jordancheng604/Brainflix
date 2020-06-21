028906888c718afa3f01c1e4c24ecd099ed1d01a
jordancheng604 <jordancheng604@gmail.com>
2020-06-21 14:07:29 -0700

    Save point: side video section for mobile done.

--- brainflix-sprint1/src/components/main/side-video/SideVideo.jsx
+++ brainflix-sprint1/src/components/main/side-video/SideVideo.jsx
@@ -10,9 +10,12 @@ export default function SideVideo({sideVids}) {
             <h5 className="SideVideo__sectionTitle" >NEXT VIDEO</h5>
             {sideVids.map(vidList => <li className="SideVideo__card" key={vidList.id}>
                 <img className="SideVideo__image" src={vidList.image} alt='thumbnail to indicate video'/>
+                <div>
                 <div className="SideVideo__title" >{vidList.title}</div>
             <div className="SideVideo__author" >{vidList.channel}</div>
+            </div>
             </li>
+            
             ).slice(0,8)}
         </ul>
     )
