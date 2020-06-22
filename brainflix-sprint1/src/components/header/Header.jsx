6c4a741ebde67c4fc384bb7795c8fd695c0c6982
jordancheng604 <jordancheng604@gmail.com>
2020-06-21 20:49:17 -0700

    Desktop: styling-Description section done.

--- brainflix-sprint1/src/components/header/Header.jsx
+++ brainflix-sprint1/src/components/header/Header.jsx
@@ -7,13 +7,12 @@ function Header(props){
         <div className='header__logo'>
         <img src={props.logo} alt='BrainFlix Logo'/>
         </div>
-        <input type='text' className="header__textSearchBox" placeholder="Search"/>
+        <input type='text' className="header__textSearchBox" placeholder='Search'/>
         <div className="header__rightSide">
            
-            {/* <img src="" alt=''/> */}
 
-            <button className="header__uploadButton"><img src={props.upPlus} alt=''/>  UPLOAD</button>
-            <img src={props.mohan} className="header__avatar" alt='mohan'/>
+        <button className="header__uploadButton"><img src={props.upPlus} alt=''/>  UPLOAD</button>
+        <img src={props.mohan} className="header__avatar" alt='mohan'/>
         </div>
         </header>
     )
