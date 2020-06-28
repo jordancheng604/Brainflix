e1f87ca3a10661a25fecbcb1d556e669fd7ead9e
jordancheng604 <jordancheng604@gmail.com>
2020-06-27 22:55:23 -0700

    Upload page: tablet styling complete.

--- brainflix-sprint2/src/components/upload/UploadComponent.jsx
+++ brainflix-sprint2/src/components/upload/UploadComponent.jsx
@@ -3,7 +3,7 @@ import uploadImage from '../../assets/images/Upload-video-preview.jpg';
 
 export default function UploadComponent() {
     return (
-        <div>
+        <form>
             <h1 className="UploadComponent__sectionTitle">Upload Video</h1>
             <h5 className="UploadComponent__videoAreaTitle">VIDEO THUMBNAIL</h5>
             <img className="UploadComponent__yourVideoImage" src={uploadImage} alt=""/>
@@ -11,8 +11,10 @@ export default function UploadComponent() {
             <input className="UploadComponent__yourTitleInput" type="text" placeholder="Add a title to your video"/>
             <h5 className="UploadComponent__yourVideoDescriptionTitle">ADD A VIDEO DESCRIPTION</h5>
             <textarea className="UploadComponent__yourVideoDetailsInput" type="text" placeholder="Add a description of your video"></textarea>
+            <div className="UploadComponent__buttonsContainer">
             <button className="UploadComponent__publishButton">PUBLISH</button>
             <button className="UploadComponent__cancelButton">CANCEL</button>
-        </div>
+            </div>
+        </form>
     )
 }
