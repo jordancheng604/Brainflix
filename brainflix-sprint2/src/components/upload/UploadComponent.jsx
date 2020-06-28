6586e9edb871191491556ccad74499626177456d
jordancheng604 <jordancheng604@gmail.com>
2020-06-27 23:33:33 -0700

    Upload page: All 3 stylings complete.

--- brainflix-sprint2/src/components/upload/UploadComponent.jsx
+++ brainflix-sprint2/src/components/upload/UploadComponent.jsx
@@ -7,10 +7,13 @@ export default function UploadComponent() {
             <h1 className="UploadComponent__sectionTitle">Upload Video</h1>
             <h5 className="UploadComponent__videoAreaTitle">VIDEO THUMBNAIL</h5>
             <img className="UploadComponent__yourVideoImage" src={uploadImage} alt=""/>
-            <h5 className="UploadComponent__yourVideoTitle">TITLE YOUR VIDEO</h5>
+            <div className="UploadComponent__yourDesktopContainer">
+            <h5 className="UploadComponent__yourVideoTitle">TITLE 
+            YOUR VIDEO</h5>
             <input className="UploadComponent__yourTitleInput" type="text" placeholder="Add a title to your video"/>
             <h5 className="UploadComponent__yourVideoDescriptionTitle">ADD A VIDEO DESCRIPTION</h5>
             <textarea className="UploadComponent__yourVideoDetailsInput" type="text" placeholder="Add a description of your video"></textarea>
+            </div>
             <div className="UploadComponent__buttonsContainer">
             <button className="UploadComponent__publishButton">PUBLISH</button>
             <button className="UploadComponent__cancelButton">CANCEL</button>
