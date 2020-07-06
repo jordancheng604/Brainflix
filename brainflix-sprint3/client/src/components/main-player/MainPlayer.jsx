2e9420e8e3e0fe07d61184efaf6e9cad2e6e1d84
jordancheng604 <jordancheng604@gmail.com>
2020-07-05 22:14:21 -0700

    Pre-cautionary save.
    Deleting and cleaning up code.

--- brainflix-sprint3/client/src/components/main-player/MainPlayer.jsx
+++ brainflix-sprint3/client/src/components/main-player/MainPlayer.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import {withRouter} from "react-router-dom";
 import playButton from "../../assets/icons/SVG/Icon-play.svg"
+import scrubber from "../../assets/icons/SVG/Icon-scrubber.svg"
+import fullscreen from "../../assets/icons/SVG/Icon-fullscreen.svg"
+import speaker from "../../assets/icons/SVG/Icon-volume.svg"
 
 function MainPlayer ({mainplay}) {
     return(
@@ -8,16 +11,23 @@ function MainPlayer ({mainplay}) {
             <video controls src="" className='mainPlayer__video' alt='Main Video' key={mainplay.id} poster={mainplay.image}>
             <source src="" type=""></source>
             </video>
-
+            <></>
             <div className='mainPlayer__controls'>
                 {/* position relative  */}
                 <div>
                 <img className='mainPlayer__playButton' src={playButton} alt=""/>
                 </div>
+                <span>
+                    <img className='mainPlayer__scrub' src={scrubber} alt=""/>
+                </span>
                 {/* //<div className='mainPlayer__playButtDiv'></div> */}
             <span className='mainPlayer__rightSide'> 
-            <span className='mainPlayer__fullScreen'></span>
-            <span className='mainPlayer__volume'></span>
+            <span className='mainPlayer__fullScreen'>
+            <img className='mainPlayer__fullscreenIMG' src={fullscreen} alt=""/>
+            </span>
+            <span className='mainPlayer__volume'>
+            <img className='mainPlayer__volumeIMG' src={speaker} alt=""/>
+            </span>
             </span>
             </div>
         </section>
